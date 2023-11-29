78e5950e420c380789c2e127deab0d6871c35fe9
Moritz <moritz.deiaco@gmail.com>
2023-11-29 07:22:15 +0100

    first commit

--- Program.cs
+++ Program.cs
@@ -1,15 +1,30 @@
+using AllWeKnow.App.Helpers;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using AllWeKnow.Data;
+using AllWeKnow.App.Services;
+using AllWeKnow.App.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+
+
+// Use Config Service
+var configService = new ConfigService();
+
+Directory.CreateDirectory(PathBuilder.Dir("storage"));
+
+builder.Services.AddScoped(typeof(Repository<>));
+
 
 var app = builder.Build();
+
+
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
