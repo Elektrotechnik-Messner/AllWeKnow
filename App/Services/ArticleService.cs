4a5abc1383de952cd38310f4cef23eb863d351b8
Moritz <moritz.deiaco@gmail.com>
2023-12-07 15:33:40 +0100

    Fixed Random Article

--- App/Services/ArticleService.cs
+++ App/Services/ArticleService.cs
@@ -5,29 +5,43 @@ namespace AllWeKnow.App.Services;
 
 public class ArticleService
 {
-    public Repository<Article> Articles;
 
-    public ArticleService(Repository<Article> articles)
+    public Repository<Article?> Articles;
+
+    private string username = null;
+
+    public ArticleService(Repository<Article?> articles)
     {
         Articles = articles;
     }
 
-    public List<Article> GetAll()
+    public List<Article?> GetAll(int status = 1)
     {
-        return Articles.Get().ToList();
+
+        return Articles.Get().Where(x => x.Status == status).ToList();
     } 
     
-    public Article GetById(int id)
+    public Article? GetById(int id)
     {
         return Articles.Get().FirstOrDefault(x => x.Id == id)!;
     }
 
-    public Article GetRandom(int? status)
-    {
-        Random random = new Random();
-
-        List<Article> articles = status is not null ? Articles.Get().Where(x => x.Status == status).ToList() : Articles.Get().ToList();
+    public Article? GetRandom(int status = 1) {
         
-        return articles[random.Next(articles.Count)];
+        Random random = new Random();
+        Article? randomArticle;
+        
+        List<int> allIds = Articles.Get().Where(x => x.Status == status).Select(x => x.Id).ToList();
+        
+        if (allIds.Count == 0) 
+            return null;
+        
+        int randomId = allIds[random.Next(allIds.Count)];
+
+        randomArticle = Articles.Get().FirstOrDefault(x => x.Id == randomId && x.Status == status)!;
+        
+        
+        return randomArticle;
+        
     }
 }
